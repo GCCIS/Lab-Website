9ec0cd2a7904841a0d305cd882b087ed8fbeb95f
acritelli <critellia@gmail.com>
2017-09-30 11:30:30 -0400

    adding events database import script

---
+++ scripts/import_db/README.md
@@ -0,0 +1,39 @@
+# Events Import Script
+
+## Overview
+This script is used to import the list of events for a week (Monday through Sunday) from api.rit.edu and insert these events into the lab website database.
+
+# Dependencies
+The script requires Python 3 and uses a number of packages that can be installed via pip (pip install -r requirements.txt)
+* Arrow: used for simplifying date manipulation
+* Requests: used to make HTTP requests to the RIT API
+* PyMySQL: used to connect to database
+arrow: pip install arrow
+makes working with dates easier
+
+# Deployment
+The script is pretty easy to install. The installation instructions below assume CentOS
+
+1. Install Python 3 and pip
+    * `yum install -y python34 python34-pip`
+2. *Optional* Create a virtual environment and activate it. If you don't do this, be sure to use Python 3 and pip 3 when using subsequent commands.
+    * `pip install virtualenv`
+    * `virtualenv -p python3 venv`
+    * `venv/bin/activate`
+3. Install the dependencies via pip
+    * `pip install -r requirements.txt`
+4. Move config.py.sample to config.py and set the relevant variables
+    * `mv config.py.sample config.py`
+    * `vim config.py`
+5. *Optional* Set up a cron job to execute the script at desired intervals so that data is continually refreshed.
+
+# Configuration
+The config.py file takes a few configuration variables:
+| Variable      | Description                                         |
+|:------------- |:--------------------------------------------------- |
+| RIT_API_KEY   | API key for api.rit.edu                             |
+| LOGGING_LEVEL | Sets the desired logging level. Default is critical |
+| DATABASE_HOST | Database hostname                                   |
+| DATABASE_NAME | Database name                                       |
+| DATABASE_USER | Username for database                               |
+| DATABASE_PW   | Password for database                               |
---
+++ scripts/import_db/import_db.py
@@ -0,0 +1,117 @@
+# TODO: try/catch here
+import arrow
+import config
+import json
+import logging
+import pymysql
+import requests
+import sys
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=config.LOGGING_LEVEL)
+logging.info('Starting database import script')
+
+# Create a database object for us to use
+db_conn = pymysql.connect(host=config.DATABASE_HOST,
+    user=config.DATABASE_USER, password=config.DATABASE_PW, db=config.DATABASE_NAME,
+    cursorclass=pymysql.cursors.DictCursor)
+
+# Get rooms from the ROOM table
+rooms = []
+try:
+    with db_conn.cursor() as cursor:
+        statement = 'SELECT * FROM ROOM;'
+        cursor.execute(statement)
+        rooms = cursor.fetchall()
+        logging.info("Retrieved list of rooms from database")
+except Exception as e:
+    logging.critical('Unable to get rooms from ROOM table ' + str(e))
+
+# Log error and exit if rooms is empty
+if len(rooms) == 0:
+    logging.critical("It seems that we didn't get any rooms back")
+    sys.exit()
+
+# Get rooms from the ROOM table
+# rooms = ['70-2160', '70-2320', '70-2520']
+rooms = ['70-2320']
+
+# Build a list of dates to iterate over
+## First get the week floor (i.e. Monday)
+utc  = arrow.utcnow()
+local = utc.to('US/Eastern')
+week_floor = local.floor('week')
+
+## Then construct an array with all of the dates that we care about (i.e. M - Sun)
+dates = [week_floor.format('YYYY-MM-DD')] # Add Monday
+for i in range(1,7): # Add remaining days
+    dates.append(week_floor.shift(days=+i).format('YYYY-MM-DD'))
+
+# For each room from the ROOM table, make an API calls to get events and create some SQL statements
+sql_statements = [] # list of sql statements that we will later execute
+for room in rooms:
+
+    # Iterate over each date to retrieve events
+    for date in dates:
+        # Build an API string of format https://api.rit.edu/v1/rooms/70-2520/meetings?date=2017-08-09&RITAuthorization=<KEY>
+        api_string = "https://api.rit.edu/v1/rooms/" + room + "/meetings?date="
+        api_string += date + "&RITAuthorization="
+        api_string_secure = api_string + config.RIT_API_KEY
+
+        # Make a request and store the data in json_data
+        try:
+            r = requests.get(api_string_secure)
+            if(r.status_code == 200):
+                logging.info('Successful request (status 200) to ' + api_string)
+                json_data = r.json()
+            else:
+                logging.critical('Non-200 status code of ' + str(r.status_code) + ' received for (sanitized) request ' + api_string)
+        except requests.exceptions.RequestException as e:
+            logging.critical('Request exception for (sanitized) request ' + api_string)
+            sys.exit()
+                
+        # Build a list of sql statements
+        try:
+            for item in json_data['data']:
+                # Get the event/meeting name. We use a split because meeting names commonly come with extra spaces (i.e. ISTE   612  01)
+                # We also truncate it if it's longer than the allowed 45 chars
+                meeting_name = ' '.join(item['meeting'].split())
+                meeting_name = meeting_name[:45]
+
+                date = item['date']
+                room_number = item['room']['building'] + '-' + item['room']['room']
+                start_time = item['start']
+                end_time = item['end']
+
+                statement = 'INSERT INTO EVENT (date, eventName, roomNumber, startTime, endTime) VALUES("%s","%s", "%s", "%s", "%s")' % (date, meeting_name, room_number, start_time, end_time)
+                sql_statements.append(statement)
+                logging.debug('Created SQL statement: ' + statement)
+        except KeyError as e:
+            logging.critical('Key error for key ' + str(e))
+            continue
+
+        # Make sure we actually have SQL statements. If not, we will log an error and exist
+        if len(sql_statements) == 0:
+            logging.critical('No SQL statements available to put into the database!')
+            sys.exit()
+
+        
+        try:
+            with db_conn.cursor() as cursor:
+                # We truncate the DB table, as long as we know that we received a response back from API.
+                statement = 'TRUNCATE TABLE EVENT'
+                cursor.execute(statement)
+
+                for statement in sql_statements:
+                    cursor.execute(statement)
+            db_conn.commit()
+        except Exception as e:
+            logging.critical('Error updating EVENT table ' + str(e))
+
+
+        # We aren't interested in tracking historical events, so this is acceptable.
+
+        # Iterate over SQL statements and insert into DB
+
+# TODO: We should be closing this connection earlier, especially if we catch an exception
+db_conn.close()
+logging.info('Database import script complete')
