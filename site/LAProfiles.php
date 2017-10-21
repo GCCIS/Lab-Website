30fb2a314974b486789b1c8defb85339159531f1
Amber Libby <amber.libby@webdev01.istlabs.rit.edu>
2017-10-21 09:30:01 -0400

    update

--- site/LAProfiles.php
+++ site/LAProfiles.php
@@ -3,7 +3,7 @@ include 'handlers/employees.php';
 include 'common/common.php';
 
 //write the HTML head - provide page title - also starts the body
-writeHTMLHead('LA Profiles');
+writeHTMLHead('Lab Assistant Profiles');
 
 //write nav
 writeNav('notActivePage','notActivePage','notActivePage','notActivePage','activePage');
