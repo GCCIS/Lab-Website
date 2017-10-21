50ae15c35c6650f3a8470232f6cd90c265666bc6
Amber Libby <amber.libby@webdev01.istlabs.rit.edu>
2017-10-21 09:33:53 -0400

    update

--- site/common/common.php
+++ site/common/common.php
@@ -98,7 +98,8 @@ function writeHTMLFooter(){
             ================================================== -->
             <!-- Placed at the end of the document so the pages load faster -->
             <script src="js/jquery.min.js"></script>
-            <script src="js/bootstrap.min.js"></script>
+            <script src="js/moment.min.js"></script>
+	    <script src="js/bootstrap.min.js"></script>
             <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
             <script src="js/ie10-viewport-bug-workaround.js"></script>
           </body>
