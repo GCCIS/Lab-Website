ec145ed608fb534dc9972a2693c933e4c8a3f63f
Amber Libby <amber.libby@webdev01.istlabs.rit.edu>
2017-11-03 11:09:11 -0400

    make the webpunch for the TAs work

---
+++ site/webpunch/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Web Punch</title>
+</head>
+
+<body>
+	<h1>TA Login</h1>
+	<form action="webPunch.php" method="post">
+		<label>Badge Number: </label>
+		<input type="text" name="TA_EID"/>
+
+		<label>Shift ends at: </label>
+		<input type="text" name="shift_end"/>
+
+		<select name="location">
+			<option value="Sys Lab">Sys Lab</option>
+			<option value="Net Lab">Net Lab</option>
+			<option value="Open Lab">Open Lab</option>
+			<option value="Other Lab">Other Lab</option>
+		</select>
+	
+		<input type="submit" value="submit">
+	</form>
+</body>
+
+</html>
+
