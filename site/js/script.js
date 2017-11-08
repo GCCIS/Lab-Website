058b24d9aa545ed7a0e2d29f76f79263d375a0b2
Chandan Mahapatra <chandan.m1992@gmail.com>
2017-11-08 15:01:25 -0500

    Calendar design fix

--- site/js/script.js
+++ site/js/script.js
@@ -1,32 +1,38 @@
 $(document).ready(function(){
 	$('#calendar').fullCalendar({
                 defaultView: 'agendaWeek',
+                //defaultView: 'basicWeek',   -- Will this work better?
                 minTime: "08:00:00",
                 maxTime: "22:00:00",
-		header: false,
+		        header: false,        
+                themeSystem: 'bootstrap3',
+                theme: 'darkly',
                 allDaySlot: false,
-		contentHeight: "auto",
-		events: {
-                	url: 'handlers/eventSchedules.php',
-                        type: 'POST', // Send post data
-                        error: function() {
-                                alert('There was an error while fetching events.');
+		        contentHeight: "auto",
+                events: {
+                            url: 'handlers/eventSchedules.php',
+                                type: 'POST', // Send post data
+                                error: function() {
+                                        alert('There was an error while fetching events.');
+                                }
                         }
-                }
 	});
 	$('#taCalendar').fullCalendar({
-		defaultView: 'agendaWeek',
-		minTime: "08:00:00",
-		maxTime: "22:00:00",
-		allDaySlot: false,
-		header: false,
-		contentHeight: "auto",
-		events: {
-			url: 'handlers/taSchedule.php',
-			type: 'POST',
-			error: function(){
-				alert('There was an error while fetching ta events');
-			}
+                defaultView: 'agendaWeek',
+                //defaultView: 'basicWeek',   -- Will this work better?
+                minTime: "08:00:00",
+                maxTime: "22:00:00",
+		        header: false,        
+                themeSystem: 'bootstrap3',
+                theme: 'darkly',
+                allDaySlot: false,
+		        contentHeight: "auto",
+                events: {
+                            url: 'handlers/taSchedule.php',
+                            type: 'POST',
+                            error: function(){
+                                alert('There was an error while fetching ta events');
+                    }
 		}
 	});
 });
