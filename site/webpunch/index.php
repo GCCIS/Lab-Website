51a0c6bdcc6a889d823cd3eec9369f53676abd12
Amber Libby <amber.libby@webdev01.istlabs.rit.edu>
2017-12-06 10:38:10 -0500

    validate and sanitize webpunch, add a little bit of css -- does not check if there is a valid kronos number

--- site/webpunch/index.php
+++ site/webpunch/index.php
@@ -1,28 +1,139 @@
 <!DOCTYPE html>
-<html lang="en">
+        <html lang="en">
+          <head>
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+            <meta name="description" content="">
+            <meta name="author" content="">
+            <link rel="icon" href="favicon.ico">
+            <title>Web Punch</title>
+            <!-- Bootstrap core CSS -->
+            <link href="../css/bootstrap.min.css" rel="stylesheet">
+            
+            <!-- Bootstrap theme CSS -->
+            <link href="../css/darkly.min.css" rel="stylesheet">
+            <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+            <link href="../css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+            
+            <!-- Custom styles for Full Calendar -->
+            <link href="../css/fullcalendar.css" rel="stylesheet" />
+              
+              
+            <!-- Custom styles for this template -->
+            <link href="../css/style.css" rel="stylesheet">
+            
+            <!-- Full Calendar, Jquery, moment Javascript file -->
+	    <script src="../js/jquery.min.js"></script>
+            <script src="../js/moment.min.js"></script>  
+            <script src="../js/fullcalendar.js"></script>
+            <!-- Just for debugging purposes. Dont actually copy these 2 lines! -->
+            <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+            <script src="../js/ie-emulation-modes-warning.js"></script>
+            <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+            <!--[if lt IE 9]>
+              <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+              <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+            <![endif]-->
+<style>
+	body{
+		padding-top: 0px;
+		
+	}
+	h1{
+		text-align: center;
+		color: #29ABE0;
+		font-weight: bold;
+	}
+	form{
+		width: 400px;
+		margin: 0 auto;
+	}
+	select{
+		color: #181818;
+	}
 
-<head>
-	<title>Web Punch</title>
-</head>
+</style>
 
-<body>
+          </head>
+          <body>
+<?php
+
+require('../handlers/DBcore.class.php');
+	function sanitize($var){
+        	$var = trim($var);
+        	$var = stripslashes($var);
+        	$var = strip_tags($var);
+        	$var = htmlspecialchars($var);
+        	return $var;
+    	}
+
+
+
+        if(isset($_POST['submitWebpunch'])){
+                //print_r($_POST);
+		//DOES NOT handle when someone enters an invalid badge number -- the database is not changed but no error is given -- the form will not show up
+                date_default_timezone_set("America/New_York");
+                $TA_EID = sanitize($_POST["TA_EID"]);
+                $shift_date = date('Y-m-d');
+                $shift_begin = date('H:i:s');
+                $shift_end = sanitize($_POST["shift_end"]).':00';
+                $location = sanitize($_POST["location"]);
+
+                //ta logged their info so add a record to the database
+                $DBcore = new DBcore();
+                $result = $DBcore->addTAshift($TA_EID, $shift_date, $shift_begin, $shift_end, $location);
+                if($result){
+                }
+                else{
+                        echo "Something went wrong -- ensure you entered a valid Badge Number";
+                }
+        }
+?>
+
+<script>
+	function validateForm(){
+		var badgeNum = document.forms["webpunchForm"]["TA_EID"].value;
+		var endTime = document.forms["webpunchForm"]["shift_end"].value;
+		if(badgeNum.length != 7){
+			alert("Badge Number must be 7 numbers long");
+			return false;
+		}
+		//^([0-1][0-9]|2[0-3]):([0-5][0-9])$
+		if(!endTime.match(/^([0-1][0-9]|2[0-3]):([0-5][0-9])$/)){
+			alert("Time must be in the format HH:MM in 24hr time")
+			return false;
+		}
+
+	}
+</script>
 	<h1>TA Login</h1>
-	<form action="webPunch.php" method="post">
+	<form action="index.php" method="post" name="webpunchForm" onsubmit="return validateForm()" >
 		<label>Badge Number: </label>
-		<input type="text" name="TA_EID"/>
-
-		<label>Shift ends at: </label>
-		<input type="text" name="shift_end"/>
-
+		<input type="text" name="TA_EID" required/>
+		<br>
+		<label>Shift ends at (use 24hr time): </label>
+		<input type="text" name="shift_end" required/>
+		<br>
+		<label>Location</label>
 		<select name="location">
 			<option value="Sys Lab">Sys Lab</option>
 			<option value="Net Lab">Net Lab</option>
 			<option value="Open Lab">Open Lab</option>
 			<option value="Other Lab">Other Lab</option>
 		</select>
-	
-		<input type="submit" value="submit">
+		<br>
+		<input type="submit" name="submitWebpunch" value="submit">
 	</form>
+
+ <!-- Bootstrap core JavaScript
+            ================================================== -->
+            <!-- Placed at the end of the document so the pages load faster -->
+	    <script src="../js/bootstrap.min.js"></script>
+            <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+            <script src="../js/ie10-viewport-bug-workaround.js"></script>
+            <script src="../js/script.js"></script>
 </body>
 
 </html>
