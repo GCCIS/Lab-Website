0789f638eae46c900a8e8a2b12cdb3525176a950
Chandan Mahapatra <chandan.m1992@gmail.com>
2017-12-15 12:19:31 -0500

    Admin Website Fix

--- site/adminsite/common/common.php
+++ site/adminsite/common/common.php
@@ -4,26 +4,35 @@
 function writeHTMLHead($title){
     echo '<!DOCTYPE html>
         <html lang="en">
-          <head>
-            <meta charset="utf-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-            <meta name="description" content="">
-            <meta name="author" content="">
-            <link rel="icon" href="favicon.ico">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
 
-            <title>'.$title.'</title>
+    <title>IST Labs</title>
 
-            <!-- Custom styles for this template -->
-            <link href="css/adminStyle.css" rel="stylesheet">
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
 
-            <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-            <!--[if lt IE 9]>
-              <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-              <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-            <![endif]-->
-          </head>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="../css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../css/adminStyle.css" rel="stylesheet">
+
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
 
           <body>';
 }//end of writeHTMLHead
@@ -41,6 +50,29 @@ function writeNav(){
               	<li><a href="logout.php">Logout</a></li>
 		</ul>
          </nav>
+         
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#"><img src="images/IST.png"></a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="activePage"><a href="admin.php">Admins</a></li>
+            <li class="notActivePage"><a href="course.php">Courses</a></li>
+            <li class="notActivePage"><a href="employee.php">Employees</a></li>
+            <li class="notActivePage"><a href="room.php">Rooms</a></li>
+            <li class="notActivePage"><a href="logout.php">Logout</a></li>  
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
 	';
 }//end of nav
 
