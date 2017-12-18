2869225b906cfcb8f481d4d902c92ac7e442380b
Amber Libby <amber.libby@webdev01.istlabs.rit.edu>
2017-12-18 08:09:49 -0500

    sanitize function

--- site/adminsite/common/common.php
+++ site/adminsite/common/common.php
@@ -73,4 +73,14 @@ function writeHTMLFooter(){
         </html>';
 }//end of writeHTMLFooter
 
+
+function sanitize($var){
+	$var = trim($var);
+	$var = stripslashes($var);
+	$var = strip_tags($var);
+	$var = htmlspecialchars($var);
+	return $var;
+}
+
+
 ?>
